b34c3e14070d39ef2724e78fc09da8603d70fea0
Hothifa Hamdan <s-hothifa.mohamed@zewailcity.edu.eg>
2024-12-10 01:27:43 +0200

    Enable CORS for all routes in the Flask application

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 import json, os
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 DATA_FILE = 'books_data.json'
 
